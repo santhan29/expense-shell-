63201338b66ee29ef641b31c85eed27d373dc732
santhan29 <santhanpeddaguravagari@gmail.com>
2024-08-29 17:50:25 +0530

    functions

--- frontend.sh
+++ frontend.sh
@@ -53,7 +53,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "extract forntend code " 
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell-/expense.conf /etc/nginx/default.d/expense.conf
 VALIDATE $? "copied expense conf"
 
 systemctl restart nginx &>>$LOG_FILE
